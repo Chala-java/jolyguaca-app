d5a458c1c52ed0ba9f469bd6319983fa062f5c60
Chala-java <chalajuandavid876@gmail.com>
2025-04-30 15:55:08 -0500

    Se implementa Encabezado

---
+++ src/components/Encabezado.jsx
@@ -0,0 +1,18 @@
+import '../components/Encabezado.css'
+
+const Encabezado = () =>{
+    return(
+        <section className="Encabezado">
+            <nav className="encabezado-nav">
+                <ul className="Encabezado-lista">
+                     <li><>Home</></li>
+                     <li><>Galeria</></li>
+                     <h1>JolyAguacateApp</h1>
+                     <li><>Proyectos</></li>
+                     <li><>Contactanos</></li>
+                </ul>
+            </nav>
+        </section>
+    )
+}
+export default Encabezado;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Card from "../components/Card";
+import Encabezado from "../components/Encabezado";
+
+const Home = () =>{
+    return(
+        <div className="cuerpo">
+            <Encabezado/>
+            <div>
+
+            <Card/>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
